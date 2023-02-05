31ae7e33a594598c765b2bf324802e16e22f3f8b
Behzad <92151678+abdOfHossein@users.noreply.github.com>
2023-02-05 23:01:02 +0330

    ok

---
+++ src/component/product.jsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+class Product extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <>
+            <span   className="m-2 text-info">product</span>
+            <span   className="m-2 badge bg-primary">2</span>
+            <button className="m-2 btn btn-sm btn-success">+</button>
+            <button className="m-2 btn btn-sm btn-warning" >-</button>
+            <button className="m-2 btn btn-sm btn-danger" >delete</button>
+            </>
+        );
+    }
+}
+ 
+export default Product;
