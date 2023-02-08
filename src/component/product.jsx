307a2843d9fdd7fe8e3e52aacd0cd0c8d0a4731e
abdOfHossein <92151678+abdOfHossein@users.noreply.github.com>
2023-02-08 09:03:05 +0330

    ok

--- src/component/product.jsx
+++ src/component/product.jsx
@@ -1,18 +1,23 @@
-import { Component } from "react";
+import { Component } from 'react';
 
 class Product extends Component {
-    state = {  } 
-    render() { 
-        return (
-            <>
-            <span   className="m-2 text-info">product</span>
-            <span   className="m-2 badge bg-primary">2</span>
-            <button className="m-2 btn btn-sm btn-success">+</button>
-            <button className="m-2 btn btn-sm btn-warning" >-</button>
-            <button className="m-2 btn btn-sm btn-danger" >delete</button>
-            </>
-        );
-    }
+  state = {};
+  render() {
+    const listTxt = ['Hi', 'How are u', 'GoodBy'];
+    return (
+      <>
+        <span className="m-2 text-info">product</span>
+        <span className="m-2 badge bg-primary">{this.format()}</span>
+        <button className="m-2 btn btn-sm btn-success">+</button>
+        <button className="m-2 btn btn-sm btn-warning">-</button>
+        <button className="m-2 btn btn-sm btn-danger">delete</button>
+        <ol>{ listTxt.map((val,index) => <li key={index}>{val}</li> )}</ol>
+      </>
+    );
+  }
+  format() {
+    return <h1>101</h1>;
+  }
 }
- 
+
 export default Product;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import ReactDOM from "react-dom";
 import Product from "./component/product";
-
 ReactDOM.render(<Product />, document.getElementById("root"));
