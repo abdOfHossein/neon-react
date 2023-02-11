d0277c526c128ad70d458549eda667dc772f5f62
Behzad <92151678+abdOfHossein@users.noreply.github.com>
2023-02-11 21:42:14 +0330

    ok

--- src/component/product.jsx
+++ src/component/product.jsx
@@ -1,22 +1,26 @@
-import { Component } from 'react';
-
+import { Component } from "react";
+import './product.css'
 class Product extends Component {
+  count = 10
+  imgUrl='https://picsum.photos/200'
   state = {};
   render() {
-    const listTxt = ['Hi', 'How are u', 'GoodBy'];
     return (
       <>
-        <span className="m-2 text-info">product</span>
-        <span className="m-2 badge bg-primary">{this.format()}</span>
-        <button className="m-2 btn btn-sm btn-success">+</button>
-        <button className="m-2 btn btn-sm btn-warning">-</button>
-        <button className="m-2 btn btn-sm btn-danger">delete</button>
-        <ol>{ listTxt.map((val,index) => <li key={index}>{val}</li> )}</ol>
+        {this.count == 0 ? (
+          <h1>Happy Meet U Dear Client</h1>
+        ) : (
+          <>
+            <span className="m-2 text-info">product</span>
+            <span className="m-2 badge bg-primary">{this.count}</span>
+            <button className="m-2 btn btn-sm btn-success">+</button>
+            <button className="m-2 btn btn-sm btn-warning">-</button>
+            <button className="m-2 btn btn-sm btn-danger">delete</button>
+            <img src={this.imgUrl}/>
+          </>
+        )}
       </>
     );
-  }
-  format() {
-    return <h1>101</h1>;
   }
 }
 
